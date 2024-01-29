5f11cc88d97928d0dd70ec8c8bc7e05f0b67f32a
Adam Kay <adam.kay@fdmgroup.com>
2024-01-29 16:24:34 +0000

    added additional error handling, custom messages and input validation

--- app/routes/api.js
+++ app/routes/api.js
@@ -12,22 +12,23 @@ const fetchFloodStationData = async (stationId) => {
     return payload
   } catch (error) {
     console.error('Error fetching flood station data', error.message)
-    throw error
+
+    const errorMessage = `Failed to fetch flood station data, the url you're trying to access is ${url}`
+    throw new Error(errorMessage)
   }
 }
 
 const fetchAssetData = async (lat, lng, radius) => {
   const url = `https://environment.data.gov.uk/asset-management/maintained-asset.geojson?lat=${lat}&lng=${lng}&radius=${radius}`
-  console.log(url)
 
   try {
     const { payload } = await Wreck.get(url, { json: true })
     return payload
   } catch (error) {
     console.error('Error fetching asset data', error.message)
-    throw new Error(
-      `Failed to fetch asset data, the url you're trying to access is ${url}`
-    )
+
+    const errorMessage = `Failed to fetch asset data, the url you're trying to access is ${url}`
+    throw new Error(errorMessage)
   }
 }
 
@@ -35,6 +36,22 @@ const fetchAssetDataFilteredById = async (request, h) => {
   try {
     const { stationId } = request.params
     const radius = request.query.radius
+
+    const { error: stationIdError } = Joi.string().max(12).validate(stationId)
+
+    if (stationIdError) {
+      const errorMessage = `Invalid stationId: ${stationIdError.message}`
+      return h.response({ error: errorMessage }).code(400)
+    }
+
+    const { error } = Joi.object({
+      radius: Joi.number().min(1.0).max(50.0).required()
+    }).validate({ radius })
+
+    if (error) {
+      const errorMessage = `Invalid radius: ${error.message}`
+      return h.response({ error: errorMessage }).code(400)
+    }
 
     const stationData = await fetchFloodStationData(stationId)
     const { lat, long } = stationData.items
@@ -46,7 +63,7 @@ const fetchAssetDataFilteredById = async (request, h) => {
     return h.response(filteredAssetIds).code(200)
   } catch (error) {
     console.error(error)
-    return h.response(error.message).code(500)
+    return h.response({ error: error.message }).code(500)
   }
 }
 
@@ -57,7 +74,9 @@ const swaggerTags = {
     params: Joi.object({
       stationId: Joi.string()
         .required()
-        .description('The station ID of the flood station of interest')
+        .max(12)
+        .description(
+          'The station ID of the flood station of interest')
     }),
     query: Joi.object({
       radius: Joi.number()
